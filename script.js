1c343d0dd4542c15441557525468c84bf7bea1b4
merittrompeter <mktrompeter@gmail.com>
2015-10-22 15:23:58 -0700

    menu

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 $( document ).ready(function() {
     console.log( "Ready!" );
+
+    $('.aboutnav').on('click', function(){
+        $('.view').addClass('hide')
+        $('.about').removeClass('hide')
+    })
+    $('.approachnav').on('click', function(){
+        $('.view').addClass('hide')
+        $('.approach').removeClass('hide')
+    })
+
+    $('.practicenav').on('click', function(){
+        $('.view').addClass('hide')
+        $('.practice').removeClass('hide')
+    })
+
+    $('.resultsnav').on('click', function(){
+        $('.view').addClass('hide')
+        $('.results').removeClass('hide')
+    })
+
 });
